0b4d7baea9b15e9d2e45566bfd3250fbe64c427f
anzhisheng <136543638+anzhisheng@users.noreply.github.com>
2024-01-24 15:00:39 -0500

    Update Main_conjoin_well_mixed.m

--- Main_conjoin_well_mixed.m
+++ Main_conjoin_well_mixed.m
@@ -8,9 +8,9 @@ madj3 = f_gen_high_mat(madj2, n);
 replace_mat = f_gen_raplace_mat(madj2, 0*madj3, n);
 repro_val = sum(replace_mat) / sum(sum(replace_mat));
 trans_mat = f_gen_trans_mat(replace_mat, n);
-retime2 = f_cal_remeet_time_two(trans_mat, n);
-retime3 = f_cal_remeet_time_three(trans_mat, retime2, n);
-bcratio = f_get_bcratio_accumulate(madj2, 0*madj3, trans_mat, repro_val, n, retime2, retime3, 0, disc1, disc2)
+retime2 = f_cal_remeet_time_two(trans_mat, n);    %% calculate two-dimensional coalescence time
+retime3 = f_cal_remeet_time_three(trans_mat, retime2, n);    %% calculate three-dimensional coalescence time
+bcratio = f_get_bcratio_accumulate(madj2, 0*madj3, trans_mat, repro_val, n, retime2, retime3, 0, disc1, disc2)    %% calculate critical ratio
 
 %%%% Higher-order interactions (mixture of second-order and third-order interactions)
 replace_mat = f_gen_raplace_mat(madj2, madj3, n);
@@ -18,5 +18,5 @@ repro_val = sum(replace_mat) / sum(sum(replace_mat));
 trans_mat = f_gen_trans_mat(replace_mat, n);
 retime2 = f_cal_remeet_time_two(trans_mat, n);
 retime3 = f_cal_remeet_time_three(trans_mat, retime2, n);
-retime4 = f_cal_remeet_time_four(trans_mat, retime2, retime3, n);
-bcratio = f_get_bcratio_accumulate(madj2, madj3, trans_mat, repro_val, n, retime2, retime3, retime4, disc1, disc2)
+retime4 = f_cal_remeet_time_four(trans_mat, retime2, retime3, n);    %% calculate four-dimensional coalescence time
+bcratio = f_get_bcratio_accumulate(madj2, madj3, trans_mat, repro_val, n, retime2, retime3, retime4, disc1, disc2)    %% calculate critical ratio
