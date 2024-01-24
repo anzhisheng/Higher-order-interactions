ae0873a08d5d5bad78b2d4ad8d06e270e36bd2b6
anzhisheng <136543638+anzhisheng@users.noreply.github.com>
2024-01-24 14:39:09 -0500

    Update Main_higher_order_star.m

--- Main_higher_order_star.m
+++ Main_higher_order_star.m
@@ -3,14 +3,14 @@ n = leaf_num * 2 + 1;
 disc1 = 1; disc2 = f_baseline_disc1_disc2(disc1);
 
 %%%% Purely pairwise interactions
-madj2 = f_gen_star_net(leaf_num);
-madj3 = f_gen_high_mat(madj2, n);
-replace_mat = f_gen_raplace_mat(madj2, 0*madj3, n);
-repro_val = sum(replace_mat) / sum(sum(replace_mat));
-trans_mat = f_gen_trans_mat(replace_mat, n);
-retime2 = f_cal_remeet_time_two(trans_mat, n);
-retime3 = f_cal_remeet_time_three(trans_mat, retime2, n);
-bcratio = f_get_bcratio_accumulate(madj2, 0*madj3, trans_mat, repro_val, n, retime2, retime3, 0, disc1, disc2)
+madj2 = f_gen_star_net(leaf_num);     %% generate pairwise structure
+madj3 = f_gen_high_mat(madj2, n);     %% generate third-order structure
+replace_mat = f_gen_raplace_mat(madj2, 0*madj3, n);      %% generate replacement graph
+repro_val = sum(replace_mat) / sum(sum(replace_mat));    %% calculate reproductive values
+trans_mat = f_gen_trans_mat(replace_mat, n);             
+retime2 = f_cal_remeet_time_two(trans_mat, n);    %% calculate two-dimensional coalescence time
+retime3 = f_cal_remeet_time_three(trans_mat, retime2, n);         %% calculate three-dimensional coalescence time
+bcratio = f_get_bcratio_accumulate(madj2, 0*madj3, trans_mat, repro_val, n, retime2, retime3, 0, disc1, disc2)   %% calculate bcratio under accumulated payoff
 
 %%%% Higher-order interactions (mixture of second-order and third-order interactions)
 replace_mat = f_gen_raplace_mat(madj2, madj3, n);
@@ -31,4 +31,3 @@ retime2 = f_cal_remeet_time_two(trans_mat, n);
 retime3 = f_cal_remeet_time_three(trans_mat, retime2, n);
 retime4 = f_cal_remeet_time_four(trans_mat, retime2, retime3, n);
 bcratio = f_get_bcratio_accumulate(0*madj2, madj3, trans_mat, repro_val, n, retime2, retime3, retime4, disc1, disc2)
-
